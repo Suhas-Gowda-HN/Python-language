8c2f938cd602b3bd1138151b161757589517346a
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2025-07-02 11:55:42 +0530

    project1

--- Day_1.py
+++ Day_1.py
@@ -1,1 +1,4 @@
 print("Welcome to the band name generator")
+A = input("What is the city you grew up in ??\n")
+B = input("what is your pet's name ??\n")
+print("Your band name could be : ",A +" "+ B)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python-language
-Learning python from zero
+Learning python from zero 
+<br>
+This will contain projects from Day 1 to Day 100 (basics to advanced)
