bb38750fdfe43b3c4c90eb5ba50485978e0ef8da
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2025-07-04 13:31:48 +0530

    project2

--- Day_2.py
+++ Day_2.py
@@ -6,6 +6,6 @@ people = int(input("How many people to split the bill? \n"))
 amount = round(((bill * tip / 100) + bill) / round(people,2), 2)
 #the f string is added and to get only 2 decimals we have used round(x,2)
 print(f"Each person should pay : {amount}")
-print("\nHave a Great Day")
+print("\nHave A Great Day")
 
 
