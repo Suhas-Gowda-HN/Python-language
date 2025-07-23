e27fb6f74218e50e0fd34a5ac1a5a5f76e20f004
Suhas-Gowda-HN <suhasgowda.aiml@gmail.com>
2025-07-23 09:59:54 +0530

    project8
    This is the encoding and decoding game , its so fun to do

---
+++ Day_8.py
@@ -0,0 +1,35 @@
+from art import logo
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+restart = True
+
+while restart is True:
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    x = input("\nwant to try again???\npress 'yes' to restart or 'no' to end: ").lower()
+    if x == "yes":
+        restart = True
+    else:
+        restart = False
+        print("\nGoodbye!")
+
+
